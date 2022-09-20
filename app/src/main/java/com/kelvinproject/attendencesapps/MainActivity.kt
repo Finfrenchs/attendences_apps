24e6d50a0ce7840f8c65014f0fa9d86531905c3d
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2022-09-20 16:18:43 +0700

    add Hitung jarak, Input Dialog Form, Setup Firebase, Send data to firebase

--- app/src/main/java/com/kelvinproject/attendencesapps/MainActivity.kt
+++ app/src/main/java/com/kelvinproject/attendencesapps/MainActivity.kt
@@ -12,15 +12,21 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.FirebaseDatabase
 import com.skyfishjy.library.RippleBackground
+import java.lang.Math.toRadians
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -145,16 +151,29 @@ class MainActivity : AppCompatActivity() {
                     val currentLat = location.latitude
                     val currentLong = location.longitude
                     val tvCheckInSuccess = findViewById<TextView>(R.id.tvCheckInSuccess)
-                    tvCheckInSuccess.visibility = View.VISIBLE
-                    tvCheckInSuccess.text = "lat: $currentLat, lon: $currentLong "
 
                     //cek sixe yang di keluarkan getAddresses
-                    Log.d("coba", "size: ${getAddresses().size}")
+                    /*Log.d("coba", "size: ${getAddresses().size}")
                     for (address: Address in getAddresses()) {
                         Log.d("coba", "lat: ${address.latitude}, lon: ${address.longitude}")
                         Log.d("coba", "postcode: ${address.postalCode}, phone: ${address.phone}")
-                    }
+                    }*/
 
+                    val distance = calculateDistance(
+                        currentLat,
+                        currentLong,
+                        getAddresses()[0].latitude,
+                        getAddresses()[0].longitude
+                    ) * 1000
+
+                    if (distance < 1000.0) {
+                        showDialogForm()
+                    } else {
+                        tvCheckInSuccess.visibility = View.VISIBLE
+                        //tvCheckInSuccess.text = "lat: $currentLat, lon: $currentLong "
+                        tvCheckInSuccess.text = "Out of range"
+                    }
+                    //Log.d("coba", "Current Location: $currentLat, $currentLong")
                     stopScanLocation()
                 }
             } else {
@@ -168,9 +187,67 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showDialogForm() {
+        val dialogForm = LayoutInflater.from(this)
+            .inflate(R.layout.layout_dialog_form, null)
+        //val etName = dialogForm.findViewById<EditText>(R.id.etName).text.toString()
+        AlertDialog.Builder(this)
+            .setView(dialogForm)
+            .setCancelable(false)
+            .setPositiveButton("Submit") { dialog, _ ->
+                val name = dialogForm.findViewById<EditText>(R.id.etName).text.toString()
+                //Toast.makeText(this, "name: $name", Toast.LENGTH_SHORT).show()
+                inputDataToFirebase(name)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun inputDataToFirebase(name: String) {
+        val user = User(name, getCurrentDate())
+
+        val database =
+            FirebaseDatabase.getInstance("https://attendences-online-apps-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val attendanceRef = database.getReference("log_attendance")
+        attendanceRef.child(name).setValue(user)
+            .addOnSuccessListener {
+                val tvCheckInSuccess = findViewById<TextView>(R.id.tvCheckInSuccess)
+                tvCheckInSuccess.visibility = View.VISIBLE
+                tvCheckInSuccess.text = "Check-In Success"
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun getCurrentDate(): String {
+        val currentTime = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault())
+        return dateFormat.format(currentTime)
+    }
+
     private fun getAddresses(): List<Address> {
-        val destination = "Balai Desa Karangrejo"
+        val destination = "Balai Besar Wilayah Sungai Brantas"
         val geocode = Geocoder(this, Locale.getDefault())
         return geocode.getFromLocationName(destination, 100)
+    }
+
+    //Hitung Jarak
+    fun calculateDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val r = 6372.8 // in kilometers
+        val radiansLat1 = toRadians(lat1)
+        val radianLat2 = toRadians(lat2)
+        val dLat = toRadians(lat2 - lat1)
+        val dLon = toRadians(lon2 - lon1)
+        return 2 * r * asin(
+            sqrt(
+                sin(dLat / 2).pow(2.0) + sin(dLon / 2).pow(2.0) * cos(radiansLat1) * cos(
+                    radianLat2
+                )
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/kelvinproject/attendencesapps/User.kt
@@ -0,0 +1,6 @@
+package com.kelvinproject.attendencesapps
+
+data class User (
+    var name: String? = null,
+    var time: String? = null
+)
